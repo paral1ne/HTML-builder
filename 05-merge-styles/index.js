f167f4db3d0c0ec411310f325b0271cafa9bbb43
Androff <rageclock23@gmail.com>
2025-01-22 15:36:27 +0100

    fix: fix incorrect copies

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const { readdir, writeFile, readFile } = require('fs/promises');
+const { createWriteStream } = require('fs');
+const { stdout } = require('process');
 
 const styleFolder = path.join(__dirname, 'styles');
 const bundleSrc = path.join(__dirname, 'project-dist', 'bundle.css');
+const writeStream = createWriteStream(bundleSrc, { flags: 'a' });
 
 async function mergeStyles() {
   const files = await readdir(styleFolder);
@@ -12,7 +15,8 @@ async function mergeStyles() {
     if (extent === '.css') {
       const filePath = path.join(styleFolder, el);
       const data = await readFile(filePath, 'utf8');
-      await writeFile(bundleSrc, data, 'utf8');
+      writeStream.write(data);
+      stdout.write(`File "${path.parse(el).base}" is comleted\n`);
     }
   });
 }
